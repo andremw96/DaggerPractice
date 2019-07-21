50d94d9eee1f1d2f41c7fab87693f6e9487a4f05
andremw96 <andre19961@gmail.com>
2019-07-21 12:20:34 +0700

    first step of dagger 2 create appcomponent and base application that will be the class APPLICATION of our apps

--- app/src/main/java/com/andreamw96/daggerpractice/BaseApplication.kt
+++ app/src/main/java/com/andreamw96/daggerpractice/BaseApplication.kt
@@ -1,12 +1,15 @@
 package com.andreamw96.daggerpractice
 
+import com.andreamw96.daggerpractice.dagger.DaggerAppComponent
 import dagger.android.AndroidInjector
 import dagger.android.support.DaggerApplication
 
+// base application class is the client  and appcomponent is the server
 class BaseApplication : DaggerApplication() {
 
+    // make AppComponent instantiate only once
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return DaggerAppComponent.builder().application(this).build()
     }
 
 }
---
+++ app/src/main/java/com/andreamw96/daggerpractice/dagger/AppComponent.kt
@@ -0,0 +1,26 @@
+package com.andreamw96.daggerpractice.dagger
+
+import android.app.Application
+import com.andreamw96.daggerpractice.BaseApplication
+import dagger.BindsInstance
+import dagger.Component
+import dagger.android.AndroidInjector
+import dagger.android.support.AndroidSupportInjectionModule
+
+@Component(
+    modules = [
+        AndroidSupportInjectionModule::class
+    ]
+)
+interface AppComponent : AndroidInjector<BaseApplication> {
+
+    @Component.Builder
+    interface Builder{
+
+        //binding application instance to the application component
+        @BindsInstance
+        fun application(application: Application) : Builder
+
+        fun build() : AppComponent
+    }
+}
